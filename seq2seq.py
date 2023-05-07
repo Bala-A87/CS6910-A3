ae097a150bcd156c3cc5ca7624109fbf2d989665
Bala-A87 <bala8702@gmail.com>
2023-05-07 22:12:56 +0530

    Add attention

--- seq2seq.py
+++ seq2seq.py
@@ -53,3 +53,41 @@ class Decoder(nn.Module):
 
     def initHidden(self):
         return torch.zeros(self.num_layers, 1, self.hidden_size)
+
+class AttnDecoder(nn.Module):
+    def __init__(
+        self,
+        hidden_size: int,
+        embedding_size: int,
+        output_size: int,
+        cell_type: nn.Module = nn.RNN,
+        num_layers: int = 1,
+        dropout: float = 0.0
+    ) -> None:
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.embedding = nn.Embedding(num_embeddings=output_size, embedding_dim=embedding_size)
+        self.attn = nn.Linear(hidden_size+embedding_size, 100)
+        self.attn_combine = nn.Linear(hidden_size+embedding_size, self.hidden_size)
+        self.decoder = cell_type(input_size=hidden_size, hidden_size=hidden_size, num_layers=num_layers, dropout=dropout)
+        self.is_lstm = False
+        self.out = nn.Linear(in_features=hidden_size, out_features=output_size)
+    
+    def forward(self, x, hidden, output_enc):
+        embedded = self.embedding(x).reshape(1, 1, -1)
+
+        attn_wts = F.softmax(self.attn(torch.cat([embedded[0], hidden[0]], 1)), dim=1)
+        attn_applied = torch.bmm(attn_wts.unsqueeze(0), output_enc.unsqueeze(0))
+
+        output = torch.cat([embedded[0], attn_applied[0]], 1)
+        output = self.attn_combine(output).unsqueeze(0)
+
+        output = F.relu(output)
+        output, hidden = self.decoder(output, hidden)
+
+        output = F.log_softmax(self.out(output[0]), dim=1)
+        return output, hidden, attn_wts
+    
+    def initHidden(self):
+        return torch.zeros(self.num_layers, 1, self.hidden_size)
--- sweep.py
+++ sweep.py
@@ -1,15 +1,16 @@
 from data import get_data, make_alphabet, word_to_tensor
-from seq2seq import Encoder, Decoder
+from seq2seq import Encoder, Decoder, AttnDecoder
 import torch
 import wandb
 
 train_data_given, train_data_target = get_data()
 val_data_given, val_data_target = get_data(split='valid')
-train_data_given, train_data_target = train_data_given[:1000], train_data_target[:1000]
-val_data_given, val_data_target = val_data_given[:1000], val_data_target[:1000]
+# train_data_given, train_data_target = train_data_given[:1000], train_data_target[:1000]
+# val_data_given, val_data_target = val_data_given[:1000], val_data_target[:1000]
 eng_alphabet = make_alphabet(train_data_given)
 tam_alphabet = make_alphabet(train_data_target)
 TFR = 0.5
+MAX_LENGTH = 100
 
 def perform_run(config=None):
     torch.cuda.empty_cache()
@@ -27,7 +28,8 @@ def perform_run(config=None):
         else:
             cell_type_class = torch.nn.RNN
         encoder = Encoder(eng_alphabet.letter_count, config.embedding_size, config.hidden_size, cell_type_class, config.encoder_layers, config.dropout if config.encoder_layers > 1 else 0.0).to(device, non_blocking=True)
-        decoder = Decoder(config.hidden_size, config.embedding_size, tam_alphabet.letter_count, cell_type_class, config.decoder_layers, config.dropout if config.decoder_layers > 1 else 0.0).to(device, non_blocking=True)
+        # decoder = Decoder(config.hidden_size, config.embedding_size, tam_alphabet.letter_count, cell_type_class, config.decoder_layers, config.dropout if config.decoder_layers > 1 else 0.0).to(device, non_blocking=True)
+        decoder = AttnDecoder(config.hidden_size, config.embedding_size, tam_alphabet.letter_count, cell_type_class, config.decoder_layers, config.dropout if config.decoder_layers > 1 else 0.0).to(device, non_blocking=True)
         optimizer_enc = torch.optim.Adam(encoder.parameters(), lr=config.lr, weight_decay=config.weight_decay)
         optimizer_dec = torch.optim.Adam(decoder.parameters(), lr=config.lr, weight_decay=config.weight_decay)
         loss_fn = torch.nn.NLLLoss()
@@ -44,19 +46,22 @@ def perform_run(config=None):
                 optimizer_enc.zero_grad()
                 optimizer_dec.zero_grad()
 
-                input_vector = word_to_tensor(eng_alphabet, train_data_given[train_index])
+                input_vector = word_to_tensor(eng_alphabet, train_data_given[train_index]).to(device, non_blocking=True)
                 input_len = len(input_vector)
+
+                outputs_enc = torch.zeros(MAX_LENGTH, encoder.hidden_size).to(device, non_blocking=True)
 
                 loss = 0.0
 
-                for char in input_vector:
+                for i, char in enumerate(input_vector):
                     if config.cell_type == 'lstm':
-                        output_enc, (hidden_enc, cell_enc) = encoder(char.to(device, non_blocking=True), (hidden_enc, cell_enc))
+                        output_enc, (hidden_enc, cell_enc) = encoder(char, (hidden_enc, cell_enc))
                     else:
-                        output_enc, hidden_enc = encoder(char.to(device, non_blocking=True), hidden_enc)
+                        output_enc, hidden_enc = encoder(char, hidden_enc)
+                    outputs_enc[i] = outputs_enc[0, 0]
                 
-                input_dec = torch.tensor([[0]])
-                target_vector = word_to_tensor(tam_alphabet, train_data_target[train_index])
+                input_dec = torch.tensor([[0]]).to(device, non_blocking=True)
+                target_vector = word_to_tensor(tam_alphabet, train_data_target[train_index]).to(device, non_blocking=True)
                 target_len = len(target_vector)
 
                 # hidden_dec = hidden_enc.to(device, non_blocking=True)
@@ -65,26 +70,26 @@ def perform_run(config=None):
                     cell_dec = torch.cat([cell_enc[-1].reshape(1, 1, -1)]*decoder.num_layers).to(device, non_blocking=True)
 
                 use_teacher_forcing = True if torch.rand(1) < TFR else False
-                prediction = []
+                prediction = ''
 
                 if use_teacher_forcing:
                     for di in range(target_len):
                         if config.cell_type == 'lstm':
-                            output_dec, (hidden_dec, cell_dec) = decoder(input_dec.to(device, non_blocking=True), (hidden_dec, cell_dec))
+                            output_dec, (hidden_dec, cell_dec), attn = decoder(input_dec, (hidden_dec, cell_dec), outputs_enc)
                         else:
-                            output_dec, hidden_dec = decoder(input_dec.to(device, non_blocking=True), hidden_dec)
-                        loss += loss_fn(output_dec, target_vector[di].to(device, non_blocking=True))
+                            output_dec, hidden_dec, attn = decoder(input_dec, hidden_dec, outputs_enc)
+                        loss += loss_fn(output_dec, target_vector[di])
                         input_dec = target_vector[di]
-                        prediction.append(output_dec.argmax(dim=1).squeeze().detach())
+                        prediction += tam_alphabet.index_to_letter[output_dec.argmax(dim=1).squeeze().detach()]
                 else:
                     for di in range(target_len):
                         if config.cell_type == 'lstm':
-                            output_dec, (hidden_dec, cell_dec) = decoder(input_dec.to(device, non_blocking=True), (hidden_dec, cell_dec))
+                            output_dec, (hidden_dec, cell_dec), attn = decoder(input_dec, (hidden_dec, cell_dec), outputs_enc)
                         else:
-                            output_dec, hidden_dec = decoder(input_dec.to(device, non_blocking=True), hidden_dec)
+                            output_dec, hidden_dec, attn = decoder(input_dec, hidden_dec, outputs_enc)
                         input_dec = output_dec.argmax(dim=1).squeeze().detach()
-                        loss += loss_fn(output_dec, target_vector[di].to(device, non_blocking=True))
-                        prediction.append(input_dec)
+                        loss += loss_fn(output_dec, target_vector[di])
+                        prediction += tam_alphabet.index_to_letter[input_dec]
                         if input_dec.item() == 1:
                             break                
 
@@ -93,7 +98,7 @@ def perform_run(config=None):
                 optimizer_dec.step()
 
                 train_loss += loss.detach() / input_len
-                if prediction == target_vector:
+                if prediction == train_data_target[train_index]+'EOW':
                     train_score += 1.
             
             train_loss /= len(train_data_given)
@@ -101,52 +106,54 @@ def perform_run(config=None):
 
             with torch.inference_mode(): 
                 for val_index in range(len(val_data_given)):
-                    input_vector = word_to_tensor(eng_alphabet, val_data_given[val_index])
+                    input_vector = word_to_tensor(eng_alphabet, val_data_given[val_index]).to(device, non_blocking=True)
                     input_len = len(input_vector)
                     hidden_enc = encoder.initHidden().to(device, non_blocking=True)
                     cell_enc = encoder.initHidden().to(device, non_blocking=True)
+                    outputs_enc = torch.zeros(MAX_LENGTH, encoder.hidden_size).to(device, non_blocking=True)
 
-                    for char in input_vector:
+                    for i, char in enumerate(input_vector):
                         if config.cell_type == 'lstm':
-                            output_enc, (hidden_enc, cell_enc) = encoder(char.to(device, non_blocking=True), (hidden_enc, cell_enc))
+                            output_enc, (hidden_enc, cell_enc) = encoder(char, (hidden_enc, cell_enc))
                         else:
-                            output_enc, hidden_enc = encoder(char.to(device, non_blocking=True), hidden_enc)
+                            output_enc, hidden_enc = encoder(char, hidden_enc)
+                        outputs_enc[i] = output_enc[0, 0]
 
-                    input_dec = torch.tensor([[0]])
-                    target_vector = word_to_tensor(tam_alphabet, val_data_target[val_index])
-                    hidden_dec = torch.cat([hidden_enc[-1].reshape(1, 1, -1)]*decoder.num_layers).to(device, non_blocking=True)
+                    input_dec = torch.tensor([[0]]).to(device, non_blocking=True)
+                    target_vector = word_to_tensor(tam_alphabet, val_data_target[val_index]).to(device, non_blocking=True)
+                    hidden_dec = torch.cat([hidden_enc[-1].reshape(1, 1, -1)]*decoder.num_layers)
                     if config.cell_type == 'lstm':
-                        cell_dec = torch.cat([cell_enc[-1].reshape(1, 1, -1)]*decoder.num_layers).to(device, non_blocking=True)
+                        cell_dec = torch.cat([cell_enc[-1].reshape(1, 1, -1)]*decoder.num_layers)
 
-                    prediction = []
+                    prediction = ''
 
-                    max_length = 100
+                    max_length = MAX_LENGTH
                     while max_length > 0:
                         if config.cell_type == 'lstm':
-                            output_dec, (hidden_dec, cell_dec) = decoder(input_dec.to(device, non_blocking=True), (hidden_dec, cell_dec))
+                            output_dec, (hidden_dec, cell_dec), attn = decoder(input_dec, (hidden_dec, cell_dec), outputs_enc)
                         else:
-                            output_dec, hidden_dec = decoder(input_dec.to(device, non_blocking=True), hidden_dec)
+                            output_dec, hidden_dec, attn = decoder(input_dec, hidden_dec, outputs_enc)
                         pred_char_index = output_dec.data.argmax()
-                        prediction.append(pred_char_index)
+                        prediction += tam_alphabet.index_to_letter[pred_char_index]
                         if pred_char_index.item() == 1:
                             break
                         input_dec = pred_char_index.squeeze().detach()
                         max_length -= 1
                     
-                    if prediction == target_vector:
+                    if prediction == val_data_target[val_index]+'EOW':
                         val_score += 1.
                 val_score /= len(val_data_given)
                 print(f'Epoch {epoch+1}/{config.epochs} => Loss: {train_loss}, Train accuracy: {train_score}, Val score: {val_score}')
             
             wandb.log({'epoch': epoch+1, 'loss': train_loss, 'accuracy': train_score, 'val_accuracy': val_score})
-            if prev_score > val_score:
+            if prev_score >= val_score:
                 break
             else:
                 prev_score = val_score
 
 sweep_config = {
     'method': 'bayes',
-    'name': 'trial'
+    'name': 'attention_sweep'
 }
 
 sweep_metric = {
@@ -187,4 +194,5 @@ parameters = {
 sweep_config['parameters'] = parameters
 
 sweep_id = wandb.sweep(sweep_config, project='CS6910-A3')
-wandb.agent(sweep_id, perform_run, project='CS6910-A3', count=1)
+# sweep_id = 'x953yurr'
+wandb.agent(sweep_id, perform_run, project='CS6910-A3')
